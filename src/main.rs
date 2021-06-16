2662ddaa56575ee2b11a1c57f716eaf5329b72ab
Vivek Malneedi <vivekmalneedi@gmail.com>
2021-06-16 15:06:30 -0400

    test tree-sitter

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
-#![recursion_limit = "256"]
+#[allow(unused)]
+mod symbol;
 
-use log::info;
-use std::sync::Arc;
-use structopt::StructOpt;
-use tower_lsp::{LspService, Server};
-
-mod completion;
-mod definition;
-mod diagnostics;
-mod format;
-mod server;
-mod sources;
-#[cfg(test)]
-mod support;
-use server::Backend;
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "veridian", about = "A SystemVerilog/Verilog Language Server")]
-struct Opt {}
-
-#[tokio::main]
-async fn main() {
-    let _ = Opt::from_args();
-    let log_handle = flexi_logger::Logger::with_str("info").start().unwrap();
-    info!("starting veridian...");
-
-    let stdin = tokio::io::stdin();
-    let stdout = tokio::io::stdout();
-
-    let (service, messages) = LspService::new(|client| Arc::new(Backend::new(client, log_handle)));
-    Server::new(stdin, stdout)
-        .interleave(messages)
-        .serve(service)
-        .await;
+fn main() {
 }
